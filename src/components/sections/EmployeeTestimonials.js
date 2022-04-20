25687d94be5ce1b20553ae4a52084cf07f4b7e4b
magnusriga <magnus.rinnan@gmail.com>
2022-04-20 23:12:28 +0200

    Fix: Testimonial speed

--- src/components/sections/EmployeeTestimonials.js
+++ src/components/sections/EmployeeTestimonials.js
@@ -88,7 +88,7 @@ export default function EmployeeTestimonials() {
         slidesToShow: isMobile ? 1 : 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 4000,
         centerMode: true,
         centerPadding: '10px',
         className: 'center',
