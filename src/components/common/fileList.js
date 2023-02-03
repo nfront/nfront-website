d1fa390bae47e9eb7bdc72857d193eb870351518
Magnus Gaarder <magnus.rinnan@gmail.com>
2023-02-03 09:40:43 +0200

    fix: files download wip

--- src/components/common/fileList.js
+++ src/components/common/fileList.js
@@ -13,9 +13,54 @@ import DescriptionIcon from '@mui/icons-material/Description';
 const FileList = ({ files, courseTitle }) => {
     const { isMobile, isLaptop, isDesktop } = useWindowSize();
     console.log('files', files);
-    
-    files.map(({id, title, filename}) => {
 
+    return files.map(({ id, title, filename, publicUrl }) => {
+        <div>
+            <FlexRow
+                gap="0"
+                alignItems="stretch"
+                className="color-grey small-font light-bold grey-border"
+            >
+                <FlexRow alignItems="center" className="px-1 py-05">
+                    <FunctionsIcon className="vertical-middle" />
+                    <span>{filename}</span>
+                </FlexRow>
+                <Link
+                    display="flex"
+                    gap="1rem"
+                    alignItems="center"
+                    className="ml-auto px-1 py-05 grey-left-border link-button normal-font-weight hover-bold"
+                >
+                    <span>{`Guide${isDesktop ? ': ' : ''}`}</span>
+                    <span
+                        className={`${
+                            isDesktop ? 'display-inline' : 'display-none'
+                        }`}
+                    >
+                        {courseTitle}
+                        <Link></Link>
+                    </span>
+                </Link>
+                <FlexRow
+                    alignItems="center"
+                    className="px-1 py-05 grey-left-border hover-scale-svg"
+                >
+                    <span
+                        className={`${
+                            isDesktop ? 'display-inline' : 'display-none'
+                        }`}
+                    >
+                        Download:
+                    </span>
+                    <button className="category--blue m-0 rounded xs-font vertical-middle">
+                        <DownloadIcon
+                            color="var(--primary-color)"
+                            className="mx-1 vertical-middle"
+                        />
+                    </button>
+                </FlexRow>
+            </FlexRow>
+        </div>;
     });
 
     return (
